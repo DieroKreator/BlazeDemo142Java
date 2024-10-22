3c6f85f61a1d7de8f808fb8ff130e01ee90072e0
DieroKreator <qtechwired@gmail.com>
2024-10-21 21:07:51 -0300

    Corrections on asserts

--- src/test/java/ComprarumapassagemTest.java
+++ src/test/java/ComprarumapassagemTest.java
@@ -1,28 +1,27 @@
 // Librarias
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.JavascriptExecutor;
-import java.util.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ComprarumapassagemTest {
   private WebDriver driver;
-  private Map<String, Object> vars;
+  // private Map<String, Object> vars;
   JavascriptExecutor js;
 
   @BeforeEach
   public void setUp() {
     driver = new ChromeDriver();
     js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
+    // vars = new HashMap<String, Object>();
   }
 
   @AfterEach
@@ -34,7 +33,7 @@ public class ComprarumapassagemTest {
   public void comprarumapassagem() {
     driver.get("https://blazedemo.com/");
     driver.manage().window().setSize(new Dimension(2560, 1318));
-    assertEquals(driver.findElement(By.cssSelector("h1")).getText(), is("Welcome to the Simple Travel Agency!"));
+    assertEquals(driver.findElement(By.cssSelector("h1")).getText(), "Welcome to the Simple Travel Agency!");
     driver.findElement(By.name("fromPort")).click();
     {
       WebElement dropdown = driver.findElement(By.name("fromPort"));
@@ -46,9 +45,9 @@ public class ComprarumapassagemTest {
       dropdown.findElement(By.xpath("//option[. = 'Rome']")).click();
     }
     driver.findElement(By.cssSelector(".btn-primary")).click();
-    assertEquals(driver.findElement(By.cssSelector("h3")).getText(), is("Flights from Philadelphia to Rome:"));
+    assertEquals(driver.findElement(By.cssSelector("h3")).getText(), "Flights from Philadelphia to Rome:");
     driver.findElement(By.cssSelector("tr:nth-child(3) .btn")).click();
-    assertEquals(driver.findElement(By.cssSelector("h2")).getText(), is("Your flight from TLV to SFO has been reserved."));
+    assertEquals(driver.findElement(By.cssSelector("h2")).getText(), "Your flight from TLV to SFO has been reserved.");
     driver.findElement(By.id("inputName")).click();
     driver.findElement(By.id("inputName")).sendKeys("John");
     driver.findElement(By.id("address")).sendKeys("123 Main st");
@@ -64,7 +63,7 @@ public class ComprarumapassagemTest {
     driver.findElement(By.id("creditCardYear")).sendKeys("2024");
     driver.findElement(By.id("nameOnCard")).sendKeys("John Test");
     driver.findElement(By.cssSelector(".btn-primary")).click();
-    assertEquals(driver.findElement(By.cssSelector("h1")).getText(), is("Thank you for your purchase today!"));
+    assertEquals(driver.findElement(By.cssSelector("h1")).getText(), "Thank you for your purchase today!");
     driver.close();
   }
 }
