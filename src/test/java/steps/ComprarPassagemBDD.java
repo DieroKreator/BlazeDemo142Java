c0414d1b8e25555a4c3d948494f698ae1954a312
DieroKreator <qtechwired@gmail.com>
2024-11-24 23:44:53 -0300

    steps implemented

--- src/test/java/steps/ComprarPassagemBDD.java
+++ src/test/java/steps/ComprarPassagemBDD.java
@@ -1,8 +1,12 @@
 package steps;
 
+import static org.junit.Assert.assertEquals;
+
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.cucumber.java.After;
@@ -16,6 +20,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class ComprarPassagemBDD {
 
     WebDriver driver;
+    String origem;
+    String destino;
 
     @Before
     public void iniciar(){
@@ -33,23 +39,35 @@ public class ComprarPassagemBDD {
     }
 
     @Dado("que acesso o site {string}")
-    public void que_acesso_o_site(String string) {
-        
+    public void que_acesso_o_site(String url) {
+        driver.get(url);
     }
 
     @Quando("seleciono a origem {string} e destino {string}")
-    public void seleciono_a_origem_e_destino(String string, String string2) {
-        
+    public void seleciono_a_origem_e_destino(String origem, String destino) {
+        {
+            this.origem = origem;
+            WebElement combo = driver.findElement(By.name("fromPort"));
+            combo.click();
+            combo.findElement(By.xpath("//option[. = '" + origem + "']")).click();
+        }
+        {
+            this.destino = destino;
+            WebElement combo = driver.findElement(By.name("toPort"));
+            combo.click();
+            combo.findElement(By.xpath("//option[. = '" + destino + "']")).click();
+        }
     }
 
     @E("clico no botao Find Flights")
     public void clico_no_botao_find_flights() {
-        
+        driver.findElement(By.cssSelector(".btn-primary")).click();
     }
 
     @Entao("visualiza a lista de voos")
     public void visualiza_a_lista_de_voos() {
-        
+        assertEquals("Flights from " + origem + " to " + destino + ":", 
+            driver.findElement(By.cssSelector("h3")).getText());
     }
 
 }
