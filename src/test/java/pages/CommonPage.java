4b140bf0fdf5b6230df477b2d0927ee58811175c
DieroKreator <qtechwired@gmail.com>
2024-11-25 23:22:15 -0300

    Created common page

---
+++ src/test/java/pages/CommonPage.java
@@ -0,0 +1,14 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CommonPage {
+
+    public WebDriver driver;
+
+    public CommonPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    // Funções em comun
+}
