006fdeeca4f17d4257db4ebda7191b9b6cf765b1
DieroKreator <qtechwired@gmail.com>
2024-12-02 16:36:21 -0300

    Added missing steps

--- src/test/java/stepsPO/ComprarPassagemPO.java
+++ src/test/java/stepsPO/ComprarPassagemPO.java
@@ -1,5 +1,30 @@
 package stepsPO;
 
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.E;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
 public class ComprarPassagemPO {
-    
+
+    @Dado("que acesso o site {string} PO")
+    public void que_acesso_o_site_po(String string) {
+    }
+
+    @Quando("seleciono a {string} e {string} PO")
+    public void seleciono_a_e_po(String string, String string2) {
+    }
+
+    @E("clico no botao Find Flights PO")
+    public void clico_no_botao_find_flights_po() {
+    }
+
+    @Entao("visualiza a lista de voos PO")
+    public void visualiza_a_lista_de_voos_po() {
+    }
+
+    @Quando("clico no {int} PO")
+    public void clico_no_po(Integer int1) {
+    }
+
 }
