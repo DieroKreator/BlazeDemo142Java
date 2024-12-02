3fe62a545219498083e9fa401d24ee7bb21722a9
DieroKreator <qtechwired@gmail.com>
2024-12-02 17:10:34 -0300

    Finished 1st step

--- src/test/java/stepsPO/ComprarPassagemPO.java
+++ src/test/java/stepsPO/ComprarPassagemPO.java
@@ -1,11 +1,14 @@
 package stepsPO;
 
+import static org.junit.Assert.assertEquals;
+
 import org.openqa.selenium.WebDriver;
 
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.E;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
+import pages.Base;
 import pages.HomePage;
 import pages.ReservePage;
 
@@ -18,9 +21,17 @@ public class ComprarPassagemPO {
     public String origem;
     public String destino;
 
+    public ComprarPassagemPO(Base base){
+        this.driver = base.driver;
+    }
+
     // As anotações Before e After ficam no Hooks.java
     @Dado("que acesso o site {string} PO")
-    public void que_acesso_o_site_po(String string) {
+    public void que_acesso_o_site_po(String url) {
+        homePage = new HomePage(driver);
+        homePage.acessarHomePage(url);
+
+        assertEquals("BlazeDemo", homePage.lerNomeDaGuia());
     }
 
     @Quando("seleciono a {string} e {string} PO")
